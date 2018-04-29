2fb70978b2e14442146e990369bb1867bfb979a3
pantieienkoooo <panmaks96@gmail.com>
2018-04-29 10:55:38 +0300

    Initial commit

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -6,6 +6,8 @@ public class BinarySearch {
     public static void main(String[] args) {
         int [] arr = {10, 20, 30, 40};
         int pos = Arrays.binarySearch(arr, 20);
+
         System.out.println(pos);
+        System.out.println("hi");
     }
 }
