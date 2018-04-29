2c93b17643e5de40a52356c2f027b469eee6ba52
pantieienkoooo <panmaks96@gmail.com>
2018-04-29 11:21:56 +0300

    Max

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -11,7 +11,8 @@ public class BinarySearch {
         System.out.println(pos);
         System.out.println(pos);System.out.println(pos);
         System.out.println(pos);System.out.println(pos);
-        
+        System.out.println(pos);System.out.println(pos);System.out.println(pos);
+
 
 
 
